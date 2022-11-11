0ad8d54c7a9d5d816ad1c9b1dc1a87b690e55ccf
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-11-11 14:47:20 +0300

    1

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,9 +1,9 @@
-'use strict'
+/*'use strict'
 let userForm = new UserForm();
 userForm.loginFormCallback = function f (data){
     if (data){
       ApiConnectors.login(data, callback);  
     }
 }
-
+*/
 
