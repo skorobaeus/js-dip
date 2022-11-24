d431c27375765c7b402684672766eb7578a49ec5
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-11-24 18:44:53 +0300

    3455

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,24 +3,14 @@
 let userForm = new UserForm();
 userForm.loginFormCallback = function f (data){
     if (data !== null){
-      ApiConnector.login({ login: data.login, password: data.password }, (response) => console.log(response));
+      ApiConnector.login({ login: data.login, password: data.password }, (response) => {console.log(response); response.success == true ? location.reload() : userForm.setLoginErrorMessage(response.error)};)
     }
-    if (response.success == true){
-        location.reload();
-    } else {
-      userForm.setLoginErrorMessage(response.error);
-    }
+    
 }
 
 userForm.registerFormCallback = function f (data) {
   if (data !== null){
-    ApiConnector.register({ login: data.login, password: data.password }, (response) => console.log(response));
-    
-  }
-if (response.success == true){
-        location.reload();
-    } else {
-      setRegisterErrorMessage(response.error);
+    ApiConnector.register({ login: data.login, password: data.password }, (response) => {console.log(response); response.success == true ? location.reload() : userForm.setLoginErrorMessage(response.error)};)
     }
 }
 
