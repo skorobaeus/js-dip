c716fb4612ad4e18bff028762f7f974403e680a3
AnatolOV <107495986+AnatolOV@users.noreply.github.com>
2022-11-25 19:57:14 +0300

    Update loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,14 +3,14 @@
 let userForm = new UserForm();
 userForm.loginFormCallback = function f (data){
     if (data !== null){
-      ApiConnector.login({ login: data.login, password: data.password }, (response) => {console.log(response); response.success == true ? location.reload() : userForm.setLoginErrorMessage(response.error)})
+      ApiConnector.login({ login: data.login, password: data.password }, (response) => {console.log(response); response.success ? location.reload() : userForm.setLoginErrorMessage(response.error)})
     }
     
 }
 
 userForm.registerFormCallback = function f (data) {
   if (data !== null){
-    ApiConnector.register({ login: data.login, password: data.password }, (response) => {console.log(response); response.success == true ? location.reload() : userForm.setLoginErrorMessage(response.error)})
+    ApiConnector.register({ login: data.login, password: data.password }, (response) => {console.log(response); response.success ? location.reload() : userForm.setLoginErrorMessage(response.error)})
     }
 }
 
